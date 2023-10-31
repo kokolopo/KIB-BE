8fb1d1e60cacd7fdccb9452c22c006e63da954b2
kokolopo <kokolopo321@gmail.com>
2023-11-01 01:40:04 +0700

    -

--- src/models/penetapan_model.js
+++ src/models/penetapan_model.js
@@ -2,6 +2,7 @@ import DB from "../config/db.js";
 
 const penetapanModel = {
   fetchByDepartemen: (idDepartemen, idKategori = 0, perPage = 10, page = 1) => {
+    const offset = (page - 1) * perPage;
     let query = `
             SELECT * FROM aset.penetapan
             WHERE departemen_id = ${idDepartemen}
@@ -9,6 +10,7 @@ const penetapanModel = {
 
     if (perPage !== "" && page !== "")
       query += `limit ${perPage} offset ${offset}`;
+
     return new Promise((resolve, reject) => {
       DB.query(query, (err, result) => {
         if (err) reject(err);
