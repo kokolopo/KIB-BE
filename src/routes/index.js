2daa6b576a33519bff3261cc8b01f37b57f410eb
kokolopo <kokolopo321@gmail.com>
2023-11-01 01:23:23 +0700

    -

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import departemen from "./departemen.js";
+import penetapan from "./penetapan.js";
 
 const routes = express.Router();
 
 routes.use("/departements", departemen);
+routes.use("/penetapan", penetapan);
 
 export default routes;
