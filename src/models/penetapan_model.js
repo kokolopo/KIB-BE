2af8f0d514d0e951d6e4622955b448977478e4e1
kokolopo <kokolopo321@gmail.com>
2023-11-04 13:39:45 +0700

    -

--- src/models/penetapan_model.js
+++ src/models/penetapan_model.js
@@ -21,7 +21,7 @@ const penetapanModel = {
   fetchTanahByDepartemen: (idDepartemen, perPage = 10, page = 1) => {
     const offset = (page - 1) * perPage;
     let query = `
-          SELECT aset.penetapan.*, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, aset.kategoris.nama
+          SELECT aset.penetapan.*,ROW_NUMBER() OVER (ORDER BY aset.penetapan.id) AS nomor, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, aset.kategoris.nama
           FROM aset.penetapan
           JOIN aset.kategoris ON aset.penetapan.kategori_id = aset.kategoris.id
           WHERE departemen_id = ${idDepartemen} AND aset.kategoris.nama LIKE 'Tanah%' AND thn_nilai = 2022
@@ -45,7 +45,7 @@ const penetapanModel = {
   ) => {
     const offset = (page - 1) * perPage;
     let query = `
-          SELECT ROW_NUMBER() OVER (ORDER BY id) AS nomor, aset.penetapan.*, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, aset.kategoris.nama
+          SELECT aset.penetapan.*, ROW_NUMBER() OVER (ORDER BY aset.penetapan.id) AS nomor, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, aset.kategoris.nama
           FROM aset.penetapan
           JOIN aset.kategoris ON aset.penetapan.kategori_id = aset.kategoris.id
           WHERE departemen_id = ${idDepartemen}
