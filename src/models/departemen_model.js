72630847294e5761d54bc333acc4616c9e07cf75
kokolopo <kokolopo321@gmail.com>
2023-11-04 12:46:09 +0700

    depart level 3

--- src/models/departemen_model.js
+++ src/models/departemen_model.js
@@ -2,9 +2,7 @@ import DB from "../config/db.js";
 
 const departemenModel = {
   fetchAll: () => {
-    let query = `
-            SELECT * FROM public.departemen
-        `;
+    let query = `SELECT * FROM public.departemen WHERE level_id = 3`;
 
     return new Promise((resolve, reject) => {
       DB.query(query, (err, result) => {
