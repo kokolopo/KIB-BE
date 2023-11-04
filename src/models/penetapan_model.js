bd31e03c6b4653bb47c8cc5a058cee86e775ee0c
kokolopo <kokolopo321@gmail.com>
2023-11-05 01:40:43 +0700

    -

--- src/models/penetapan_model.js
+++ src/models/penetapan_model.js
@@ -64,7 +64,8 @@ const penetapanModel = {
   ) => {
     const offset = (page - 1) * perPage;
     let query = `
-    SELECT p.*, k.nama, ROW_NUMBER() OVER (ORDER BY p.id) AS nomor, TO_CHAR(p.tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated
+    SELECT p.*, k.nama, ROW_NUMBER() OVER (ORDER BY p.id) AS nomor, TO_CHAR(p.tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated,
+    (SELECT COUNT(*) FROM aset.penetapan) AS total_data
     FROM aset.penetapan AS p
     JOIN public.departemen AS d ON p.departemen_id = d.id
     JOIN aset.kategoris AS k ON p.kategori_id = k.id
