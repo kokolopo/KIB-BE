392f2ea1e576092e58b66fcf5130391a6395485d
kokolopo <kokolopo321@gmail.com>
2023-11-04 13:24:41 +0700

    tgl foremated

--- src/models/penetapan_model.js
+++ src/models/penetapan_model.js
@@ -3,14 +3,12 @@ import DB from "../config/db.js";
 const penetapanModel = {
   fetchDataById: (id) => {
     let query = `
-      SELECT aset.penetapan.*, public.departemen.kode, public.departemen.nama, aset.kategoris.kode AS barang, aset.kategoris.nama AS nama_barang
+      SELECT aset.penetapan.*, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, public.departemen.kode, public.departemen.nama, aset.kategoris.kode AS barang, aset.kategoris.nama AS nama_barang
       FROM aset.penetapan
       JOIN public.departemen ON aset.penetapan.departemen_id = public.departemen.id
       JOIN aset.kategoris ON aset.penetapan.kategori_id = aset.kategoris.id
       WHERE aset.penetapan.id = ${id}
     `;
-
-    // let query = `select *, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated from aset.penetapan WHERE aset.penetapan.id = ${id}`;
 
     return new Promise((resolve, reject) => {
       DB.query(query, (err, result) => {
@@ -23,7 +21,7 @@ const penetapanModel = {
   fetchTanahByDepartemen: (idDepartemen, perPage = 10, page = 1) => {
     const offset = (page - 1) * perPage;
     let query = `
-          SELECT aset.penetapan.*, aset.kategoris.nama
+          SELECT aset.penetapan.*, TO_CHAR(tgl_perolehan, 'DD-MM-YYYY') AS tgl_perolehan_formated, aset.kategoris.nama
           FROM aset.penetapan
           JOIN aset.kategoris ON aset.penetapan.kategori_id = aset.kategoris.id
           WHERE departemen_id = ${idDepartemen} AND aset.kategoris.nama LIKE 'Tanah%' AND thn_nilai = 2022
