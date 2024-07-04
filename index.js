4c229b26ae5b51e6025ec1e2dd1ab0deb7e4552e
kokolopo <kokolopo321@gmail.com>
2024-07-05 01:05:57 +0700

    as

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({ credentials: true }));
+app.use(cors());
+// app.use(cors({ credentials: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
