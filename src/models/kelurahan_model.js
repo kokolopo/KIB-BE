1f53282e4efc909dc98f061cbeddb98a54339b40
kokolopo <kokolopo321@gmail.com>
2024-07-16 21:04:34 +0700

    kelurahan

--- src/models/kelurahan_model.js
+++ src/models/kelurahan_model.js
@@ -1,24 +1,25 @@
 import DB from "../config/db.js";
 
 const kelurahanModel = {
-    fetchAll: (id_kecamatan) => {
-        let query = `
-            SELECT 
-                kel.kode as kelurahan_kd,
-                kel.nama as kelurahan_nm
+  fetchAll: (id_kecamatan) => {
+    let query = `
+            SELECT
+                kel.id,
+            kel.nama as kelurahan_nm,
+            kel.kode || ' - ' || kel.nama as value
             FROM 
-                aset_manfaat.kelurahan AS kel
-            WHERE
+                aset_manfaat.kelurahan as kel
+            where
                 kel.kecamatan_id = ${id_kecamatan}
         `;
 
-        return new Promise((resolve, reject) => {
-        DB.query(query, (err, result) => {
-            if (err) reject(err);
-            resolve(result.rows);
-        });
-        });
-    },
+    return new Promise((resolve, reject) => {
+      DB.query(query, (err, result) => {
+        if (err) reject(err);
+        resolve(result.rows);
+      });
+    });
+  },
 };
 
 export default kelurahanModel;
